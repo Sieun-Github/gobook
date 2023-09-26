2019c70e2b47ce300db3f50a7bf5e9ba5c27eb9e
sieun-Github <sieun6205@naver.com>
2023-09-26 19:10:48 +0900

    초기 프로젝트 세팅

---
+++ android/app/src/main/kotlin/com/sudarlife/gobook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sudarlife.gobook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ gobook/android/app/src/main/kotlin/com/example/gobook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gobook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
